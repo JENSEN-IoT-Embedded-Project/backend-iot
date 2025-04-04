bc97c268b0161279fa821a9a17026150656fc6eb
benflo <benji@DESKTOP-JQVMOHH.>
2025-04-04 20:54:21 +0200

    first draft httpserver in c

---
+++ benji/c-server/client.py
@@ -0,0 +1,26 @@
+import socket
+
+# Skapa en TCP-socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Anslut till servern (här antas servern köras på localhost och port 8080)
+client_socket.connect(('localhost', 8080))
+
+# Skapa HTTP GET-förfrågan
+http_request = """GET /index.html HTTP/1.1\r\n\
+Host: localhost:8080\r\n\
+User-Agent: PythonSocketClient\r\n\
+Accept: text/html\r\n\
+Connection: close\r\n\
+\r\n"""
+
+# Skicka HTTP GET-förfrågan till servern
+client_socket.sendall(http_request.encode())
+
+# Ta emot och skriv ut serverns svar
+response = client_socket.recv(4096)
+print("Server Response:")
+print(response.decode())
+
+# Stäng anslutningen
+client_socket.close()
---
+++ benji/c-server/http_server.c
@@ -0,0 +1,81 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+
+
+// FUNCTIONS
+
+
+void get_http_method(char* str){
+	printf("GHELO");
+	char temp_str[8]; // make room for OPTIONS & CONNECT
+	int i = 0;
+	for(i; i < 8; i++){
+		if(str[i] == ' '){
+			break;
+		}else{
+			temp_str[i] = str[i];
+		}
+	}
+	temp_str[i] = '\0';
+	printf("%s", temp_str);
+}
+
+
+int main(){
+
+	// GLOBALS
+
+	const int PORT = 8080;
+
+	struct sockaddr_in address = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(PORT)
+	};
+
+	// SERVER SETUP
+
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_fd == -1){
+		perror("Server socket creation failed\n");
+		return 1;
+	}
+
+	int server_did_bind = bind(server_fd,(struct sockaddr *)&address, sizeof(address));
+	if(server_did_bind == -1){
+		perror("Bind failed\n");
+		return 1;
+	}
+
+	int server_listening = listen(server_fd, 10);
+	if(server_listening == -1){
+		printf("Listen failed\n");
+		return 1;
+	}else{
+		printf("Server is listening on port %d\n", PORT);
+	}
+
+
+	// MAIN LOOP
+
+	while(1){
+		char buffer[1024] = {0};
+		int client_fd = accept(server_fd, (struct sockaddr *)&address, &(socklen_t){sizeof(address)});
+		ssize_t recieved_bytes = read(client_fd, buffer, sizeof(buffer) - 1);
+		if(recieved_bytes < 0){
+			perror("Read msg from client failed\n");
+		}else{
+			printf("tah");
+			get_http_method(buffer);
+			printf("Hello");
+
+		}
+		close(client_fd);
+	}
+	close(server_fd);
+	return 0;
+}
